051afb52fbdd049e3d651502690d5baa003990b3
Zaidkhan0407 <zaidk.051103@gmail.com>
2025-04-30 21:50:58 +0530

    Sumsquare

--- Practice.py
+++ Practice.py
@@ -1,7 +1,13 @@
-def fact(n):
+def factorial(n):
     if n == 0:
         return 1
     if n == 1:
         return 1
     return n * fact(n - 1)
 print(fact(5))
+
+def sumSquare(n):
+    if n<=0: 
+        return 1
+    return (n**2+sumSquare(n-1))
+print(sumSquare(2))
