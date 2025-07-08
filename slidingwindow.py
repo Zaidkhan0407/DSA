0a33c1c0f0fa3eba60fe720cae4c3c3eac2c1695
Zaidkhan0407 <zaidk.051103@gmail.com>
2025-07-08 20:25:57 +0530

    8 jull

--- slidingwindow.py
+++ slidingwindow.py
@@ -19,3 +19,21 @@ def length_of_longest_substring_k_distinct(s, k):
 s = "eceba"
 k = 2
 print(length_of_longest_substring_k_distinct(s, k))
+
+def maxsubarr(nums, target):
+    if not nums:
+        return 0
+    n=len(nums)
+    i=0
+    curr_sum=0
+    min_len=float("inf")
+    for j in range(n):
+        curr_sum+=nums[j]
+        while curr_sum>=target:
+            min_len=min(min_len,j-i+1)
+            curr_sum-=nums[i]
+            i+=1
+    return min_len if min_len !=float("inf") else 0
+
+nums=[2, 3, 1, 2, 7, 3]
+print(maxsubarr(nums, 7))
